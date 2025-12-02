7aec267fb111fd41ce841f8ed2e9b37e88ee8ace
Gerardo Baez <g@gerardobaez.com>
2025-12-02 18:01:20 -0400

    Refactor parameter name in `BillingProvider::create` for consistency.

--- src/Provider/BillingProvider.php
+++ src/Provider/BillingProvider.php
@@ -6,7 +6,7 @@ use AlturaCode\Billing\Core\Subscriptions\Subscription;
 
 interface BillingProvider
 {
-    public function create(Subscription $draft, array $options = []): BillingProviderResult;
+    public function create(Subscription $subscription, array $options = []): BillingProviderResult;
 
     public function cancel(Subscription $subscription, bool $atPeriodEnd, array $options): BillingProviderResult;
 
