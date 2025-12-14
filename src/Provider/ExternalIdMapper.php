2ceee730d93bd49e31a7727d3f30c8a2faa5d736
Gerardo Baez <g@gerardobaez.com>
2025-12-14 00:52:04 -0400

    Update `ExternalIdMapper` interface to support `string|int` for IDs and add `storeMultiple` method
    - Enhanced typing for `internalId` and `externalId` parameters to allow `string|int` in all methods.
    - Introduced `storeMultiple` method for batch ID mapping operations.

--- src/Provider/ExternalIdMapper.php
+++ src/Provider/ExternalIdMapper.php
@@ -6,27 +6,30 @@ namespace AlturaCode\Billing\Core\Provider;
 
 interface ExternalIdMapper
 {
-    public function store(string $type, string $provider, array $internalId, string $externalId): void;
+    public function store(string $type, string $provider, string|int $internalId, string|int $externalId): void;
 
+    /**
+     * @param array<array{type: string, provider: string, internalId: string|int, externalId: string|int}> $data
+     */
     public function storeMultiple(array $data): void;
 
-    public function getExternalId(string $type, string $provider, string $internalId): string|null;
+    public function getExternalId(string $type, string $provider, string|int $internalId): string|int|null;
 
     /**
      * @param string $type
      * @param string $provider
-     * @param array<string> $internalIds
-     * @return array<string>
+     * @param array<string|int> $internalIds
+     * @return array<string|int>
      */
     public function getExternalIds(string $type, string $provider, array $internalIds): array;
 
-    public function getInternalId(string $type, string $provider, string $externalId): string|null;
+    public function getInternalId(string $type, string $provider, string|int $externalId): string|int|null;
 
     /**
      * @param string $type
      * @param string $provider
-     * @param array<string> $externalIds
-     * @return array<string>
+     * @param array<string|int> $externalIds
+     * @return array<string|int>
      */
     public function getInternalIds(string $type, string $provider, array $externalIds): array;
 }
