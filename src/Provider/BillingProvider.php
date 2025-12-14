d319aa1c1a0d1d124c7cc19bf12f0c3d75f0eb7f
Gerardo Baez <g@gerardobaez.com>
2025-12-14 03:04:54 -0400

    Remove redundant empty lines in `BillingProvider` interface

--- src/Provider/BillingProvider.php
+++ src/Provider/BillingProvider.php
@@ -3,15 +3,9 @@
 namespace AlturaCode\Billing\Core\Provider;
 
 use AlturaCode\Billing\Core\Subscriptions\Subscription;
-use AlturaCode\Billing\Core\Subscriptions\SubscriptionItem;
 
 interface BillingProvider
 {
     public function create(Subscription $subscription, array $options = []): BillingProviderResult;
-
-
-
     public function cancel(Subscription $subscription, bool $atPeriodEnd, array $options): BillingProviderResult;
-
-
 }
